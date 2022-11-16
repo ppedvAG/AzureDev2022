f321ff89dcc5cc44103d0d9ffb00fece1b09c19d
André <AndreR@ppedv.de>
2022-11-16 13:25:40 +0100

    Logger App

---
+++ LoggerApp/LoggerApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LoggerApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.Debug()
+                            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Minute)
+                            .WriteTo.Seq("http://20.113.5.155/")
+                            .CreateLogger();
+
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log.Fatal("Crash {@Error}", e.ExceptionObject);
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ LoggerApp/LoggerApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoggerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInfo(object sender, RoutedEventArgs e)
+        {
+            Log.Information("Log Info {@OS}", Environment.OSVersion.Version);
+        }
+
+        private void LogFatal(object sender, RoutedEventArgs e)
+        {
+            throw new OutOfMemoryException();
+        }
+
+        private void LogError(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                File.OpenRead("b:\\plqöekfmweflknm.txt");
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error("Log Error {@error}", ex);
+            }
+        }
+    }
+}
